57300f58986ec2a3f88e0535b18a04e0c9f312c2
aokingAK <GitHub>
2023-05-22 20:45:46 +0900

    change provider2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrap();
 
         \Illuminate\Support\Facades\Schema::defaultStringLength(191);
+
+        if(\App::environment(['pridction'])){
+           \URL::forceScheme('https');
+        }
     }
 }
