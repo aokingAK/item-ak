d3665f1b35bb3b479e0b8f7e927b37f9049d5bde
aokingAK <GitHub>
2023-05-28 20:44:35 +0900

    change

--- resources/views/item/end.blade.php
+++ resources/views/item/end.blade.php
@@ -25,6 +25,7 @@
                     <table class="table table-hover text-nowrap table table-striped table-bordered text-center">
                         <thead>
                             <tr>
+                                <th>ID</th> 
                                 <th>発注者</th>
                                 <th>名前</th>
                                 <th>カテゴリー</th>
@@ -37,6 +38,7 @@
                         <tbody>
                             @foreach ($confirms as $confirm)
                                 <tr>
+                                    <td>{{ $confirm->id }}</td>                                   
                                     <td>{{ $confirm->username }}</td>
                                     <td>{{ $confirm->name }}</td>
                                     <td>{{ App\Models\Item::TYPES[$confirm->type_id] }}</td>
